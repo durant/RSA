717e2d1ed46a8e4ff536d1da1be5605c1ba6c3a8
durant <magicboy317327966@163.com>
2016-09-15 01:42:10 +0800

    完善macOS下加密

--- RSAForMac/RSADemo/ViewController.m
+++ RSAForMac/RSADemo/ViewController.m
@@ -43,22 +43,6 @@
 
 
 - (IBAction)perforClick:(id)sender {
-//    NSString *baseStr = @"fw+ekwQV/+cHq2idXMzD1q4B+RPxpwbmyEBcYz2epKEvjQDSDMwOSkkB23kp+ZVgs88UkN5DBAEqGfoaEe4HZd0l/oGRHlfH+dG7n3Hx5mRdRkVWz2Yz6vn/uH8KJjsEV81YmbBWrDlruLpPCIlfEACWTbnF3lOcTr7RdwRuZ01xrNdMbsjOYhU695LUWrVEav58h4WXrGqiytwVfnPzQ2iDk3dlv7X2eFDGOTibLdLwqeR/n+E9obUddWUMreDodzlf0EyynKOwaAeA4feNamVxaWnlY8DVbT5PPB5Py8qrHiLs1FKuHHxakpwEzQ9EzixFaUOo+ZYqLrbxqwBuUw==";
-//    NSData *encryData = [CCMBase64 dataFromBase64String:baseStr];
-//    CCMCryptorTest *test = [[CCMCryptorTest alloc] init];
-//    CCMPublicKey *publicKey = [test loadPublicKeyResource:@"rsa_public_key"];
-//    CCMPrivateKey *privateKey = [test loadPrivateKeyResource:@"rsa_private_key"];
-//    CCMCryptor *cryptor = [[CCMCryptor alloc] init];
-//    NSError *error;
-//        NSData *decryptedData = [cryptor decryptData:encryData
-//                                      withPrivateKey:privateKey
-//                                               error:&error];
-//        NSString *output = [[NSString alloc] initWithData:decryptedData
-//                                                 encoding:NSUTF8StringEncoding];
-//        NSLog(@"%@ ", output);
-//        
-//        return;
-
     NSOpenPanel *panel   = [NSOpenPanel openPanel];
     panel.canChooseFiles = true;
     panel.canChooseDirectories = false;
@@ -69,7 +53,7 @@
     {
         NSString *path = [[panel URL] path];
         
-        self.filePath  = path;
+        //self.filePath  = path;
         
         self.pathField.stringValue = path;
         
@@ -81,28 +65,22 @@
 }
 
 - (IBAction)addSignAction:(id)sender {
-    CCMCryptorTest *test = [[CCMCryptorTest alloc] init];
-    CCMPublicKey *publicKey = [test loadPublicKeyResource:@"rsa_public_key"];
+    CCMCryptorTest *test      = [[CCMCryptorTest alloc] init];
+    CCMPublicKey *publicKey   = [test loadPublicKeyResource:@"rsa_public_key"];
     CCMPrivateKey *privateKey = [test loadPrivateKeyResource:@"rsa_private_key"];
-    CCMCryptor *cryptor = [[CCMCryptor alloc] init];
+    CCMCryptor *cryptor       = [[CCMCryptor alloc] init];
     NSError *error;
-
-    NSString *originString = @"hello world!";
-    for(int i=0; i<4; i++){
-        originString = [originString stringByAppendingFormat:@" %@", originString];
-    }
     
-    NSData *inputData = [originString dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *path    = [[NSBundle mainBundle] pathForResource:@"jspathDemo" ofType:@"js"];
+    NSData *inputData = [[NSData alloc] initWithContentsOfFile:path];
     
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"jspathDemo" ofType:@"js"];
-    inputData = [[NSData alloc] initWithContentsOfFile:path];
-    
-    // RSA 签名
-    NSData *signData = [cryptor signData:inputData withPrivateKey:privateKey error:&error];
+    // RSA 签名 with private key
+    NSData *signData  = [cryptor signData:inputData withPrivateKey:privateKey error:&error];
     NSString *signStr = [CCMBase64 base64StringFromData:signData];
     
     NSLog(@"signStr == %@ ", signStr);
     
+    // RSA 验证签名 with public key
     BOOL f = [cryptor verifySignData:signData data:inputData withPublicKey:publicKey error:&error];
     if(f)
     {
@@ -112,26 +90,21 @@
     {
         NSLog(@" 签名失败");
     }
-    // This can be decrypted on the command line with the following command:
-    // % echo -n "<output>" \
-    //   | base64 -D \
-    //   | openssl rsautl -decrypt -inkey test/resources/private_key.pem
+    
     NSData *encryptedData ;
-    NSString *baseStr;
-    encryptedData  = [cryptor encryptData:inputData withPublicKey:publicKey error:&error];
+    NSString *encryptedStr;
+    NSData *decryptedData;
+    NSString *output;
     
-    baseStr = [CCMBase64 base64StringFromData:encryptedData];
-
-    NSLog(@"%@ ", baseStr);
+    // rsa 加密 with public key
+    encryptedData = [cryptor encryptData:inputData withPublicKey:publicKey error:&error];
+    encryptedStr  = [CCMBase64 base64StringFromData:encryptedData];
+    NSLog(@"%@ ", encryptedStr);
     
-    encryptedData = [CCMBase64 dataFromBase64String:baseStr];
-    NSData *decryptedData = [cryptor decryptData:encryptedData
-                                  withPrivateKey:privateKey
-                                           error:&error];
-    NSString *output = [[NSString alloc] initWithData:decryptedData
-                                             encoding:NSUTF8StringEncoding];
+    // rsa 解密 with private key
+    decryptedData = [cryptor decryptData:encryptedData withPrivateKey:privateKey error:&error];
+    output        = [[NSString alloc] initWithData:decryptedData encoding:NSUTF8StringEncoding];
     NSLog(@"%@ ", output);
-
 }
 
 - (void)setRepresentedObject:(id)representedObject {
